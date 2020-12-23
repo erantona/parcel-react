94b4a24b2fc3f4b7443c14ac13c07bf64c9e63b4
erantona <roysuvendu1557.gmail.com>
2020-12-23 14:22:36 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './styles/style.css';
+const App = () => {
+  return (
+    <div>
+      <h1>Lkoso</h1>
+      <p>hotwing</p>
+    </div>
+  );
+};
+
+export default App;
+// "build:css": "postcss src/styles/tailwind.css -o src/styles/style.css"
